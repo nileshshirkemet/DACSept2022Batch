6f3c747c1aa5603c673c98d162c66acb92e8202c
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-10-14 17:43:35 +0530

    Delete Java/Language/InterfaceTest1 directory

--- Java/Language/InterfaceTest1/src/IllegalTransferException.java
+++
@@ -1,5 +0,0 @@
-package edu.met.banking;
-
-//unchecked exception can go unreported
-public class IllegalTransferException extends RuntimeException {}
-
--- Java/Language/InterfaceTest1/src/Profitable.java
+++
@@ -1,9 +0,0 @@
-package edu.met.banking;
-
-public interface Profitable {
-
-	double interest(int months);
-
-	float MIN_RATE = 4.0f;
-}
-
