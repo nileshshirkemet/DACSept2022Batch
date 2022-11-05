b06770f29ad3677ba8aa58ba7d6a9aaa0e53b56c
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-11-05 11:50:07 +0530

    Add files via upload

---
+++ dotnet/Language/Inheritance/InterfaceTest2/DemoApp/Program1.cs
@@ -0,0 +1,54 @@
+﻿using Taxation;
+
+class Program
+{
+    //nested class - can only refer to static members of outer class
+    class Auditor
+    {
+        internal Auditor()
+        {
+            Console.WriteLine("Acquiring resources.");
+        }
+
+        public void Audit(string id, ITaxPayer person)
+        {
+            if(id.Length < 4)
+                throw new ArgumentException("Invalid ID");
+            double payment = person.IncomeTax() + 500;
+            Console.WriteLine("Total Payment: {0:0.00}", payment);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing resources.");
+        }
+    }
+
+    private static void Process(string name, int count)
+    {
+        ITaxPayer t = name == "jack" ? new Engineer(count) : new Programmer(count);
+        Auditor a = new Auditor();
+        try
+        {
+            a.Audit(name, t);
+        }
+        finally
+        {
+            a.Dispose();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string m = args[0].ToLower();
+            int n = int.Parse(args[1]);
+            Process(m, n);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ dotnet/Language/Inheritance/InterfaceTest2/DemoApp/Program2.cs
@@ -0,0 +1,49 @@
+﻿using Taxation;
+
+class Program
+{
+    //nested class - can only refer to static members of outer class
+    class Auditor : IDisposable
+    {
+        internal Auditor()
+        {
+            Console.WriteLine("Acquiring resources.");
+        }
+
+        public void Audit(string id, ITaxPayer person)
+        {
+            if(id.Length < 4)
+                throw new ArgumentException("Invalid ID");
+            double payment = person.IncomeTax() + 500;
+            Console.WriteLine("Total Payment: {0:0.00}", payment);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing resources.");
+        }
+    }
+
+    private static void Process(string name, int count)
+    {
+        ITaxPayer t = name == "jack" ? new Engineer(count) : new Programmer(count);
+        using(Auditor a = new Auditor())
+        {
+            a.Audit(name, t);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            string m = args[0].ToLower();
+            int n = int.Parse(args[1]);
+            Process(m, n);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
