e724bc8a61735e58ec13e55d5ce4c8e071b4cd34
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-11-14 12:07:44 +0530

    Add files via upload

---
+++ dotnet/Platform/Communication/TcpClientTest/DemoApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+
+string item = args[0].ToLower();
+int quantity = int.Parse(args[1]);
+string host = args[2];
+using TcpClient client = new TcpClient(host, 4010);
+using Stream channel = client.GetStream();
+using StreamReader reader = new StreamReader(channel);
+using StreamWriter writer = new StreamWriter(channel);
+Console.WriteLine(reader.ReadLine());
+writer.WriteLine(item);
+writer.Flush();
+string response = reader.ReadLine();
+if(response is not null)
+{
+    ItemInfo info = ItemInfo.Parse(response);
+    if(quantity <= info.Stock)
+        Console.WriteLine("Total Payment: {0:0.00}", quantity * info.Price);
+    else
+        Console.WriteLine("Item out of stock!");
+}
+else
+{
+    Console.WriteLine("Item not sold!");
+}
