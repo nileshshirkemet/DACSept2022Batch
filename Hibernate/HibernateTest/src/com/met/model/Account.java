9c8f4521756978c72ab1c5075d32788af4210247
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-12-24 17:54:49 +0530

    Add files via upload

---
+++ Hibernate/HibernateTest/src/com/met/model/Account.java
@@ -0,0 +1,44 @@
+package com.met.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Account {
+
+	@Id
+	private int id;
+	private String name;
+	private double balance;
+	
+	@Transient
+	private double interest;
+	
+	public double getInterest() {
+		return interest;
+	}
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+		
+	
+}
