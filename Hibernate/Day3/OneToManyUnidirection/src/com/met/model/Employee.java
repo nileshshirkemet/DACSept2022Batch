f41ace94fbe5e1f5e36e36d4fecd40b56106280b
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-12-30 19:23:50 +0530

    Add files via upload

---
+++ Hibernate/Day3/OneToManyUnidirection/src/com/met/model/Employee.java
@@ -0,0 +1,65 @@
+package com.met.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Employee {							//Source ENtity
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(length = 12)
+	private String name;
+	
+	@Column(length = 20)
+	private String emailId;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "fk_emp_id")
+	private List<Address> listAddress;				//Target ENtity
+
+	public List<Address> getListAddress() {
+		return listAddress;
+	}
+
+	public void setListAddress(List<Address> listAddress) {
+		this.listAddress = listAddress;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", emailId=" + emailId + "]";
+	}
+	
+	
+	
+}
